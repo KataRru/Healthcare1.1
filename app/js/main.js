0b8346357a95a0cf411be19fb0d572744214e7bf
Kataru <belogorod5@gmail.com>
2024-10-17 13:17:41 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,54 @@
+//SLADER
+$(document).ready(function(){
+  $('.slider__items').slick({
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+  });
+});
+
+
+//BURGER 
+const burger = document.querySelector('.burger'),
+  menu = document.querySelector('.burger__menu'),
+  closeElem = document.querySelector('.burger__menu-close')
+
+  burger.addEventListener('click', () => {
+    menu.classList.add('active');
+  });
+
+  closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+
+
+//SCROLL BAR 
+$(document).ready(() => {
+  $('.burger').click(function(event) {
+    $('body').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.burger__menu-close').click(function(event) {
+    $('body').removeClass('active');
+  });
+});
